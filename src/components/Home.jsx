e91de29c7872e705db40be7f4555bba94bf0e0c4
Webbney Vallon <webbvallon1999@gmail.com>
2025-07-22 11:13:48 -0400

    navbar changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,32 @@
 import React from "react";
+// import "/.HomeStyles.css";
 
+// const Home = () => {
+//   return (
+//     <div>
+//       <h1>Hello React!</h1>
+//       <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
+//     
+//     </div>
+//   );
+// };
 const Home = () => {
   return (
-    <>
-      <h1>Hello React!</h1>
-      <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
-    </>
+    <div className="app-container">
+      <img 
+        className="coolphoto" 
+        src="/cool.jpg" 
+        alt="Fullscreen background" 
+      />
+      <div className="content">
+        <h1>Hello React!</h1>
+        <img className="react-logo" src="/react-logo.svg" alt="React Logo" />
+      </div>
+    </div>
   );
 };
 
+
 export default Home;
-// edit this page 
+
+
