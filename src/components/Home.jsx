7e1aa1c0016f8ee015036a0be561d5f969de4a15
Elian Echavarria <elianstanly431@gmail.com>
2025-07-23 13:53:59 -0400

    Home Page CSS Done ✅

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 // import homeBG from "../assets/images/homeBG.jpg";
 import "../styles/HomeStyles.css";
 import { useEffect } from "react";
+import voteIcon from "../assets/images/voteIcon.png";
 
 const Home = () => {
   useEffect(() => {
@@ -32,6 +33,41 @@ const Home = () => {
           <div className="s1">1</div>
           <div className="s2">1</div>
           <div className="s3">1</div>
+          <div className="s4">2</div>
+          <div className="s5">2</div>
+          <div className="s6">2</div>
+          <div className="s7">3</div>
+          <div className="s8">3</div>
+          <div className="s9">3</div>
+          <div className="s10">4</div>
+          <div className="s11">4</div>
+          <div className="s12">4</div>
+          <div className="s13">5</div>
+          <div className="s14">5</div>
+        </div>
+      </div>
+      <div className="vote-container">
+        <div className="vote-title-flex">
+          <p className="container-title-vote">Vote</p>
+          <img
+            src={voteIcon}
+            alt="Vote Icon"
+            className="container-second-title"
+          />
+        </div>
+        <hr />
+        <div className="voting-grid-wrapper">
+          <div className="voting-grid">
+            <div className="v1"></div>
+            <div className="v2"></div>
+            <div className="v3"></div>
+            <div className="v4"></div>
+            <div className="v5"></div>
+            <div className="v6"></div>
+          </div>
+        </div>
+        <div className="btn-container">
+          <button className="vote-home-btn">Vote Now!</button>
         </div>
       </div>
     </>
