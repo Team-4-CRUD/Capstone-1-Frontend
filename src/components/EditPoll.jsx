f1f57f382bd509ba4d3e479bb812c47791744d7b
Elian Echavarria <elianstanly431@gmail.com>
2025-07-27 17:06:15 -0400

    Added css for Edit ✅

--- src/components/EditPoll.jsx
+++ src/components/EditPoll.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
-import "../styles/EditStyles.css";
 import axios from "axios";
+import "../styles/EditStyles.css";
 
 const EditPoll = () => {
   const { id } = useParams();
@@ -111,110 +111,239 @@ const EditPoll = () => {
 
   return (
     <>
-      <h1 className="edit-poll-title">Edit Poll {id}</h1>
-      <div className="form-container">
-        <form onSubmit={handleSubmit}>
-          <div className="edit-header-fields">
-            <label>Poll Title</label>
-            <input
-              type="text"
-              name="title"
-              placeholder="Pick a Title"
-              value={editFormData.title}
-              onChange={handleChange}
-              required
-            />
+      <div className="split-screen-Edit">
+        <div className="left-side-Edit"></div>
+        <div className="right-side-Edit">
+          <h1 className="edit-poll-title">Edit Poll {id}</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="edit-header-fields">
+              <label>Poll Title:</label>
 
-            <label>Poll Description</label>
-            <input
-              type="text"
-              name="description"
-              placeholder="Write a Description"
-              value={editFormData.description}
-              onChange={handleChange}
-              required
-            />
-            <input
-              type="checkbox"
-              checked={editFormData.private || false}
-              onChange={(e) =>
-                setEditFormData((prevData) => ({
-                  ...prevData,
-                  private: e.target.checked,
-                }))
-              }
-              name="AuthUser"
-            />
-            <label htmlFor="AuthUser">
-              Allow only authenticated users to vote?
-            </label>
-          </div>
-
-          {editFormData.pollElements.map((el) => (
-            <div key={el.id}>
               <input
                 type="text"
-                name="option"
-                placeholder="Pick an Option"
-                value={el.option}
-                onChange={(e) =>
-                  handleChange(
-                    e,
-                    editFormData.pollElements.findIndex(
-                      (opt) => opt.id === el.id
-                    )
-                  )
-                }
+                name="title"
+                placeholder="Pick a Title"
+                value={editFormData.title}
+                onChange={handleChange}
                 required
               />
+            </div>
+            <div className="description-edit-field">
+              <label>Poll Description:</label>
               <input
                 type="text"
-                name="info"
-                placeholder="Write some Info"
-                value={el.info}
-                onChange={(e) =>
-                  handleChange(
-                    e,
-                    editFormData.pollElements.findIndex(
-                      (opt) => opt.id === el.id
-                    )
-                  )
-                }
+                name="description"
+                placeholder="Write a Description"
+                value={editFormData.description}
+                onChange={handleChange}
                 required
               />
-              <input
-                type="url"
-                name="picture"
-                placeholder="Choose a picture"
-                value={el.picture}
-                onChange={(e) =>
-                  handleChange(
-                    e,
-                    editFormData.pollElements.findIndex(
-                      (opt) => opt.id === el.id
-                    )
-                  )
-                }
-                required
-              />
+            </div>
+            <div className="auth-btn-container">
               <button
                 type="button"
-                className="delete-option-button"
-                onClick={() => handleDeleteElement(el.id)}
-                disabled={editFormData.pollElements.length <= 2}
+                className={`auth-btn${editFormData.private ? " active" : ""}`}
+                onClick={() =>
+                  setFormData((prevData) => ({
+                    ...prevData,
+                    private: !prevData.private,
+                  }))
+                }
+                style={{
+                  background: editFormData.private ? "#709255" : "#ffffff",
+                  color: editFormData.private ? "#fff" : "#333",
+                  border: "1px solid #709255",
+                  borderRadius: "20px",
+                  padding: "1rem",
+                  cursor: "pointer",
+                  fontWeight: "bold",
+                  fontFamily: "Outfit",
+                  width: "380px",
+                  marginBottom: "40px",
+                }}
               >
-                ❌
+                {editFormData.private
+                  ? "Auth-Only Voting: ON"
+                  : "Auth-Only Voting: OFF"}
               </button>
             </div>
-          ))}
 
-          <input type="submit" value="Update Poll" />
-          <button type="button" onClick={handleAddElement}>
-            Add Option
-          </button>
-        </form>
+            {editFormData.pollElements.map((el, idx) => (
+              <div key={el.id} className="options-container-edit">
+                <label htmlFor="option">Poll Option {idx + 1}:</label>
+                <input
+                  type="text"
+                  name="option"
+                  placeholder="Pick an Option"
+                  value={el.option}
+                  onChange={(e) =>
+                    handleChange(
+                      e,
+                      editFormData.pollElements.findIndex(
+                        (opt) => opt.id === el.id
+                      )
+                    )
+                  }
+                  required
+                />
+                <input
+                  type="text"
+                  name="info"
+                  placeholder="Write some Info"
+                  value={el.info}
+                  onChange={(e) =>
+                    handleChange(
+                      e,
+                      editFormData.pollElements.findIndex(
+                        (opt) => opt.id === el.id
+                      )
+                    )
+                  }
+                  required
+                />
+                <input
+                  type="url"
+                  name="picture"
+                  placeholder="Choose a picture"
+                  value={el.picture}
+                  onChange={(e) =>
+                    handleChange(
+                      e,
+                      editFormData.pollElements.findIndex(
+                        (opt) => opt.id === el.id
+                      )
+                    )
+                  }
+                  required
+                />
+                <div className="delete-option-button">
+                  <button
+                    type="button"
+                    className="delete-option-button"
+                    onClick={() => handleDeleteElement(el.id)}
+                    disabled={editFormData.pollElements.length <= 2}
+                  >
+                    Delete Option
+                  </button>
+                </div>
+              </div>
+            ))}
+            <div className="update-poll-container">
+              <input type="submit" value="Update Poll" />
+              <button type="button" onClick={handleAddElement}>
+                Add Option
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
+
+    //  </div>
+    //   <h1 className="edit-poll-title">Edit Poll {id}</h1>
+    //   <div className="form-container">
+    //     <form onSubmit={handleSubmit}>
+    //       <div className="edit-header-fields">
+    //         <label>Poll Title</label>
+    //         <input
+    //           type="text"
+    //           name="title"
+    //           placeholder="Pick a Title"
+    //           value={editFormData.title}
+    //           onChange={handleChange}
+    //           required
+    //         />
+
+    //         <label>Poll Description</label>
+    //         <input
+    //           type="text"
+    //           name="description"
+    //           placeholder="Write a Description"
+    //           value={editFormData.description}
+    //           onChange={handleChange}
+    //           required
+    //         />
+    //         <input
+    //           type="checkbox"
+    //           checked={editFormData.private || false}
+    //           onChange={(e) =>
+    //             setEditFormData((prevData) => ({
+    //               ...prevData,
+    //               private: e.target.checked,
+    //             }))
+    //           }
+    //           name="AuthUser"
+    //         />
+    //         <label htmlFor="AuthUser">
+    //           Allow only authenticated users to vote?
+    //         </label>
+    //       </div>
+
+    //       {editFormData.pollElements.map((el) => (
+    //         <div key={el.id}>
+    //           <input
+    //             type="text"
+    //             name="option"
+    //             placeholder="Pick an Option"
+    //             value={el.option}
+    //             onChange={(e) =>
+    //               handleChange(
+    //                 e,
+    //                 editFormData.pollElements.findIndex(
+    //                   (opt) => opt.id === el.id
+    //                 )
+    //               )
+    //             }
+    //             required
+    //           />
+    //           <input
+    //             type="text"
+    //             name="info"
+    //             placeholder="Write some Info"
+    //             value={el.info}
+    //             onChange={(e) =>
+    //               handleChange(
+    //                 e,
+    //                 editFormData.pollElements.findIndex(
+    //                   (opt) => opt.id === el.id
+    //                 )
+    //               )
+    //             }
+    //             required
+    //           />
+    //           <input
+    //             type="url"
+    //             name="picture"
+    //             placeholder="Choose a picture"
+    //             value={el.picture}
+    //             onChange={(e) =>
+    //               handleChange(
+    //                 e,
+    //                 editFormData.pollElements.findIndex(
+    //                   (opt) => opt.id === el.id
+    //                 )
+    //               )
+    //             }
+    //             required
+    //           />
+    //           <button
+    //             type="button"
+    //             className="delete-option-button"
+    //             onClick={() => handleDeleteElement(el.id)}
+    //             disabled={editFormData.pollElements.length <= 2}
+    //           >
+    //             ❌
+    //           </button>
+    //         </div>
+    //       ))}
+
+    //       <input type="submit" value="Update Poll" />
+    //       <button type="button" onClick={handleAddElement}>
+    //         Add Option
+    //       </button>
+    //     </form>
+    //   </div>
+    // </>
   );
 };
 
